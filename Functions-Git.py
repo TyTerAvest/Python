00cb004ba1ab141104c584ef3ef247fb965bcd0d
Ty Ter Avest <02ty.teravest@gulllakecs.org>
2019-03-12 12:29:14 -0400

    Name & age

--- Functions-Git.py
+++ Functions-Git.py
@@ -62,3 +62,16 @@ def print_numbers(x):
 
 print_number(13)
 print_number(23)
+
+
+# 4.14.4: Name & age
+# Ty Ter Avest
+# 2.18.19
+
+def name_and_age(name, age):
+    print('\n', 'Hi, my name is', name, 'and I am', age, 'years old!')
+
+name_and_age('Mike', 33)
+name_and_age('Zane', 18)
+
+print('Git Commit')
